8c5bce210722e551119077e04d226ea320fd7806
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-12-31 01:09:09 +0400

    wired up createUser function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const createUser = require('./create_user.js')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.create_user = functions.https.onRequest(createUser)
